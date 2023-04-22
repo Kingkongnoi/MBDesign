b0099845918a2bb7dc9332a792e3f0d1d4cf2e45
Wannaporn Yaemngamluea <zzc0413407@celestica.com>
2023-04-22 10:03:59 +0700

    commit : create front-end in 3D design

---
+++ MBDesignWeb/Controllers/Design3DController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MBDesignWeb.Controllers
+{
+    public class Design3DController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
