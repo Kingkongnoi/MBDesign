956c8fa653695ec256cffe2eccac727d63a16c8a
Wannaporn Yaemngamluea <zzc0413407@celestica.com>
2023-06-04 09:00:35 +0700

    commit fixed error in rdlc

--- ReportRDLC/Program.cs
+++ ReportRDLC/Program.cs
@@ -16,7 +16,6 @@ namespace ReportRDLC
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
         }
     }
 }
