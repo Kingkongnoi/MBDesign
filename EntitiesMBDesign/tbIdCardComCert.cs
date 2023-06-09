5ea9e07f2d66a7b9baba891500ec4a4c1e1a4628
Wannaporn Yaemngamluea <zzc0413407@celestica.com>
2023-06-09 10:17:34 +0700

    commit : add entities upload id card module

---
+++ EntitiesMBDesign/tbIdCardComCert.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiesMBDesign
+{
+    public class tbIdCardComCert
+    {
+        [Key]
+        public int id { get; set; }
+        public string idCardComCertId { get; set; } = string.Empty;
+
+    }
+}
