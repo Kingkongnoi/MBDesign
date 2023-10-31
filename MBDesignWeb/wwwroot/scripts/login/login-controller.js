a60dd07db66331d7c04b27461be38e499a836abd
Wannaporn Yaemngamluea <zzc0413407@celestica.com>
2023-10-31 12:25:32 +0700

    commit : add enter event in login page and comment tab commission and bonus

--- MBDesignWeb/wwwroot/scripts/login/login-controller.js
+++ MBDesignWeb/wwwroot/scripts/login/login-controller.js
@@ -7,4 +7,16 @@
     $('.btn-login').on('click', function () {
         login();
     });
+
+    $('#input-login-empCode').on('keypress', function (e) {
+        if (e.which == 13) {
+            $('#input-login-empPass').focus();
+        }
+    });
+
+    $('#input-login-empPass').on('keypress', function (e) {
+        if (e.which == 13) {
+            $('.btn-login').trigger('click');
+        }
+    });
 })
